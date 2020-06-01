26448b4ec08908e29ef9eb29aca7a0e7d105f87b
David Murillo <davimuri@gmail.com>
2020-06-01 17:30:47 +0200

    Add

---
+++ The_Black_Eyed_Peas-I_Gotta_Feeling.rb
@@ -0,0 +1,25 @@
+4.times do
+  play chord(:G, :M)
+  sleep 0.5
+end
+2.times do
+  play chord(:G, :sus4)
+  sleep 0.5
+end
+2.times do
+  play chord(:G, :M)
+  sleep 0.5
+end
+6.times do
+  play chord(:C, :M)
+  sleep 0.5
+end
+2.times do
+  play chord(:D, :M)
+  sleep 0.5
+end
+6.times do
+  play chord(:E, :m)
+  sleep 0.5
+end
+
